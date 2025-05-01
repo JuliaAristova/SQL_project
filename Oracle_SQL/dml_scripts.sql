addc1636930c914be5fb64313f0a759bc52673b7
JuliaAristova <39885876+JuliaAristova@users.noreply.github.com>
2025-05-01 09:48:35 -0400

    Delete Oracle_SQL/dml_scripts.sql

--- Oracle_SQL/dml_scripts.sql
+++
@@ -1,7 +0,0 @@
---add data to a table 
-INSERT INTO table_name (col_1, col_2, col_3...)
-VALUES (value_1, value_2, value_3 ...)
-
---see inserted value
-SELECT *                    --or list of columns
-FROM table_name
