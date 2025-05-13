ad670650df97107d843f3b72f49dfffe6a66b20d
JuliaAristova <39885876+JuliaAristova@users.noreply.github.com>
2025-05-13 13:14:32 -0400

    Update script_createTable.sql

--- Oracle_SQL/2_Using_DDL/script_createTable.sql
+++ Oracle_SQL/2_Using_DDL/script_createTable.sql
@@ -10,21 +10,24 @@ CREATE TABLE work_schedule (
   end_date            DATE
 );
 
+
+CREATE TABLE cruises (
+  cruise_id           NUMBER,                          # name   data type
+  cruise_type_id      NUMBER,
+  cruise_name         VARCHAR(20),
+  captain_id          NUMBER NOT NULL,                  #  unnamed constraint
+  start_date          DATE,
+  end_date            DATE,
+  status              VARCHAR(5)  DEFAULT 'DOCK'
+  CONSTRAINT          PRIMARY KEY (cruise_id)          # unique, not null
+  );
+
 /*
 columns - 7
 constrains  - 2
   captain_id - unnamed constraint, will be assigned system-generated name
   primary key - unique value for each cruis_id
-default value will be used if not provided in INSERT
+
+status column - DEFAULT value will be used if not provided in INSERT
 */
-CREATE TABLE cruises (
-  cruise_id           NUMBER,
-  cruise_type_id      NUMBER,
-  cruise_name         VARCHAR(20),
-  captain_id          NUMBER NOT NULL,
-  start_date          DATE,
-  end_date            DATE,
-  status              VARCHAR(5)  DEFAULT 'DOCK'
-  CONSTRAINT          PRIMARY KEY (cruise_id)
-  );
  
