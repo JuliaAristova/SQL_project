d153f8e5cec5b0c3674b7e74c942c5a7048f0e68
JuliaAristova <39885876+JuliaAristova@users.noreply.github.com>
2025-06-24 10:42:45 -0400

    Update and rename script_createTable.sql to script_create_alter_Table.sql

--- Oracle_SQL/2_Using_DDL/script_create_alter_Table.sql
+++ Oracle_SQL/2_Using_DDL/script_create_alter_Table.sql
@@ -39,3 +39,56 @@ Output:
 2 column - Null  - shows whether there is a NOT NULL constraint applied to that particular column in the table
 3 colum - Type  - shows the data tyoe for the particular table's column
 */
+
+create table positions(
+position_id    NUMBER Primary Key,
+deptno	       NUMBER references dept(deptno) ON DELETE CASCADE,	
+position       VARCHAR2(20) CONSTRAINT pos_nn NOT NULL,
+exempt         VARCHAR2(10) CHECK (exempt IN ('Y', 'N')),
+notes          VARCHAR2(50) DEFAULT 'Add description',
+abbr	       VARCHAR2(5) CONSTRAINT abbr_u UNIQUE
+);
+
+create table positions(
+position_id    NUMBER,
+deptno	       NUMBER,	
+position       VARCHAR2(20),
+exempt         VARCHAR2(10),
+notes          VARCHAR2(50) DEFAULT 'Add description',
+abbr	       VARCHAR2(5),
+CONSTRAINT pos_pk PRIMARY KEY (position_id),
+CONSTRAINT deptno_fk FOREIGN KEY(deptno) REFERENCES dept(deptno) ON DELETE SET NULL,
+UNIQUE (abbr)
+);
+
+ ALTER TABLE positions
+ MODIFY position NOT NULL;
+
+ ALTER TABLE positions
+ ADD CONSTRAINT exempt_c CHECK (exempt IN ('Y', 'N'));
+
+ ALTER TABLE positions
+ DROP CONSTRAINT exempt_c;
+
+ ALTER TABLE positions
+ RENAME CONSTRAINT SYS_CO011155 TO  pos_nn;
+
+ALTER TABLE positions
+ ADD col_1 NUMBER;
+
+ALTER TABLE positions
+ ADD (
+	col_1 NUMBER,
+	col2  NUMBER
+);
+
+
+ALTER TABLE positions
+ DROP COLUMN col_only_one;
+
+ALTER TABLE positions
+ DROP (col_1);
+
+ALTER TABLE positions
+ DROP (col_1, col_2);
+
