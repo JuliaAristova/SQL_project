19874c8aaf4b7d5509ce5375dba9131fdd9b78a4
JuliaAristova <39885876+JuliaAristova@users.noreply.github.com>
2025-06-30 12:20:06 -0400

    Update dml_script.sql

--- Oracle_SQL/1_Oracle_SQL/dml_script.sql
+++ Oracle_SQL/1_Oracle_SQL/dml_script.sql
@@ -1,8 +1,13 @@
-
 --add data to a table 
 INSERT INTO table_name (col_1, col_2, col_3...)
-VALUES (value_1, value_2, value_3 ...)
+VALUES (value_1, value_2, value_3 ...);
+
+-- add to all columns
+  INSERT INTO table_name VALUES (value_1, value_2, value_3 ...);
+
+  -- add to specific colums (all constraints should be followed)
+  INSERT INTO table_name (col_5, col_1) VALUES (val_5, val_1);
 
 --see inserted value
 SELECT *                    --or list of columns
-FROM table_name
+FROM table_name;
